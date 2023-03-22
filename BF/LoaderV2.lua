37f35a212787c3340d652f0f78a3c16b256578ce
BinbinvoGaming <123051097+BinbinvoGaming@users.noreply.github.com>
2023-03-22 18:44:19 +0700

    Update LoaderV2.lua

--- BF/LoaderV2.lua
+++ BF/LoaderV2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/LoliHubV2/053c866592e500719bf36609683cf2c6a5b5a220/BinbinvoGamingHubAutoFarmV2.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/LoliHubV2/c5140be70ffa2f4484bf0598fa3ba0f8dc68e6cf/BinbinvoGamingHubAutoFarmV2.lua'))()
